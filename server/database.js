70098854a5da8a3e8f66938b00d45072bc6bfd7e
Dioceus <chuwei.chen2001@gmail.com>
2022-03-17 20:14:44 -0400

    setup db

---
+++ server/database.js
@@ -0,0 +1,51 @@
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database('./anime.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) 
+    {
+        console.error(err.message);
+    }
+    else
+    {
+        console.log("Connected to the anime database");
+        db.run(`CREATE TABLE voice_actor(
+                    name text PRIMARY KEY
+                );
+                CREATE TABLE voiced(
+                    actor_name text,
+                    anime_id INTEGER,
+                    character_name text,
+                    primary key(actor_name, anime_id)
+                );
+                CREATE TABLE anime(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title text,
+                    genre text,
+                    studio text
+                );
+                CREATE TABLE watched(
+                    anime_id INTEGER,
+                    username text,
+                    primary key(anime_id, username)
+                );
+                CREATE TABLE user(
+                    username text, PRIMARY KEY,
+                    name text,
+                    password text
+                );
+                `,
+            (err) => {
+                if (err)
+                {
+                    console.log("Table already created");
+
+                }
+                else 
+                {
+                    console.log("Table created");
+                }
+            });
+    }
+});
+
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const app = express();
-
-const sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database('./anime.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    else
-    {
-        console.log("Connected to the anime database");
-    }
-});
+let db = require("./database.js");
 
 const PORT = 5000;
 
