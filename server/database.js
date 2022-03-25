e33da55d0186407467dcb86d57bcd93061f5399e
Dioceus <chuwei.chen2001@gmail.com>
2022-03-25 00:38:13 -0400

    add prelimary data

--- server/database.js
+++ server/database.js
@@ -45,7 +45,52 @@ let db = new sqlite3.Database('./anime.db', sqlite3.OPEN_READWRITE, (err) => {
 
                     const anime_insert = "INSERT into anime (title, genre, studio) values (?,?,?)";
 
+                    //31 anime
+
+                    db.run(anime_insert, ["Weathering With You", "Romance, Fantasy", "CoMix Wave Films"]);
+                    db.run(anime_insert, ["Your Name", "Romance, Fantasy", "CoMix Wave Films"]);
+                    db.run(anime_insert, ["Violet Evergarden", "Coming of Age, Romance, Fantasy", "Kyoto Animation"]);
+                    db.run(anime_insert, ["Rascal Does Not Dream of Bunny Girl Senpai", "Psychological Drama, Romance, Supernatural", "CloverWorks"]);
+                    db.run(anime_insert, ["Your Lie In April", "Romantic Drama", "A-1 Pictures"]);
+                    db.run(anime_insert, ["Death Note", "Mystery, Phychological Thriller, Supernatural Thriller", "Madhouse"]);
+                    db.run(anime_insert, ["Clannad", "Slice of Life, Romance", "Kyoto Animation"]);
+                    db.run(anime_insert, ["Clannad After Story", "Slice of Life, Romance, Tragedy", "Kyoto Animation"]);
+                    db.run(anime_insert, ["Anohana: The Flower We Saw That Day", "Coming of Age", "A-1 Pictures"]);
+                    db.run(anime_insert, ["Grave of the Fireflies", "War, Drama, Tragedy", "Studio Ghibli"]);
+                    db.run(anime_insert, ["KonoSuba: God's Blessing on this Wonderful World!", "Isekai, Fantasy Comedy", "Studio Deen"]);
+                    db.run(anime_insert, ["Quintessential Quintuplets", "Harem, Romantic Comedy", "Tezuka Productions"]);
+                    db.run(anime_insert, ["Spirited Away", "Fantasy, Adventure, Coming of Age, Mystery", "Studio Ghibli"]);
+                    db.run(anime_insert, ["Puella Magi Madoka Magica", "Dark Fantasy, Psychological Thriller, Magical girl", "Shaft"]);
+                    db.run(anime_insert, ["A Silent Voice", "Romance, Drama", "Kyoto Animation"]);
+                    db.run(anime_insert, ["Kaguya-sama: Love Is War", "Psychological, Romantic Comedy, Slice of Life", "A-1 Pictures"]);
+                    db.run(anime_insert, ["Angel Beats", "Fantasy", "P.A.Works"]);
+                    db.run(anime_insert, ["Plastic Memories", "Comedy Drama, Romance, Science Fiction", "Doga Kobo"]);
+                    db.run(anime_insert, ["I Want To Eat Your Pancreas", "Coming of Age, Drama, Romance", "Studio VOLN"]);
+                    db.run(anime_insert, ["Golden Time", "Romantic Comedy", "J.C.Staff"]);
+                    db.run(anime_insert, ["Tsuki ga Kirei", "Romance, Coming of Age", "Feel"]);
+                    db.run(anime_insert, ["Re:Zero - Starting Life in Another World", "Isekai, Adventure, Dark Fantasy", "White Fox"]);
+                    db.run(anime_insert, ["Demon Slayer: Kimetsu no Yaiba", "Action, Adventure, Dark Fantasy", "Ufotable"]);
+                    db.run(anime_insert, ["Hotarubi No Mori e", "Romance, Supernatural", "Brain's Base"]);
+                    db.run(anime_insert, ["Made in Abyss", "Adventure, Dark Fantasy, Science Fiction", "Kinema Citrus"]);
+                    db.run(anime_insert, ["5 Centimeters per Second", "Romance, Drama, Melodrama", "CoMix Wave Inc."]);
+                    db.run(anime_insert, ["Beastars", "Coming of Age, Drama, Fantasy", "Orange"]);
+                    db.run(anime_insert, ["Rent A Girlfriend", "Romantic Comedy", "TMS Entertainment"]);
+                    db.run(anime_insert, ["Domestic Girlfriend", "Romantic Drama", "Diomedéa"]);
+                    db.run(anime_insert, ["The Garden of Woods", "Drama, Romance", "CoMix Wave Films"]);
+                    db.run(anime_insert, ["Attack on Titan", "Action, Dark Fantasy, Post-apocalyptic", "Wit Studio, MAPPA"]);
+
                     const voiceActor_insert = "INSERT into voice_actor values (?)";
+
+                    db.run(voiceActor_insert, "Kana Hanazawa"); 
+                    db.run(voiceActor_insert, "Yui Ishikawa");
+                    db.run(voiceActor_insert, "Natsuki Hanae");
+                    db.run(voiceActor_insert, "Sora Amamiya");
+                    db.run(voiceActor_insert, "Rie Takahashi");
+                    db.run(voiceActor_insert, "Ai Kayano");
+                    db.run(voiceActor_insert, "Yoshitsugu Matsuoka");
+                    db.run(voiceActor_insert, "Ayane Sakura");
+                    db.run(voiceActor_insert, "Inori Minase");
+
 
                     const voiced_insert = "INSERT into voiced values (?, ?, ?)";
 
