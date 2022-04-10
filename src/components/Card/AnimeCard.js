f03b1302542d5622cc4caa8398d31c0cf6140dc7
Dioceus <chuwei.chen2001@gmail.com>
2022-04-09 21:33:40 -0400

    display N:N relationship

--- src/components/Card/AnimeCard.js
+++ src/components/Card/AnimeCard.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import { Card,Button } from 'react-bootstrap';
 
 const AnimeCard = (props) => {
 
+    const [user, setUser] = useState([]);
+    const [showWatched, setShowWatched] = useState(true);
+
+    const submitUser = () => {
+        if (showWatched)
+        {
+            axios.get(`/api/watched/anime/${props.title}`)
+            .then(response => {
+                
+                setShowWatched(false); 
+
+                const responseData = response.data;
+                
+                setUser(responseData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        }
+        else
+        {
+            setUser([]);
+            setShowWatched(true);
+        }
+    }
+
+    let watched = [];
+
+    user.forEach(eachUser => {
+        watched.push(<Card.Text>{eachUser.username}</Card.Text>)
+    });
+
     return(
         <Card>
             <Card.Title> 
-                <Button variant = "outline-dark">
+                <Button variant = "outline-dark" onClick = {submitUser}>
                     {props.title}
                 </Button>
             </Card.Title>
 
             <Card.Text>Genre: {props.genre}</Card.Text>
             <Card.Text>Studio: {props.studio}</Card.Text>
+
+            {watched.length > 0 ? <Card.Title>Watched: </Card.Title> : null}
+            {watched}
         </Card>
     );
 }
--- src/components/Card/UserCard.js
+++ src/components/Card/UserCard.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import { Card, Button } from 'react-bootstrap';
 
 const UserCard = (props) => {
+
+    const [anime, setAnime] = useState([]);
+    const [showWatched, setShowWatched] = useState(true);
+
+    const submitAnime = () => {
+        if (showWatched)
+        {
+            axios.get(`/api/watched/user/${props.username}`)
+            .then(response => {
+                
+                setShowWatched(false); 
+
+                const responseData = response.data;
+                
+                setAnime(responseData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        }
+        else
+        {
+            setAnime([]);
+            setShowWatched(true);
+        }
+    }
+
+    let watched = [];
+
+    anime.forEach(eachAnime => {
+        watched.push(<Card.Text>{eachAnime.anime_title}</Card.Text>)
+    });
 
     return(
         <Card>
             
             <Card.Title>
-                <Button variant = "outline-dark">
+                <Button variant = "outline-dark" onClick = {submitAnime}>
                     {props.username}
                 </Button>
             </Card.Title>
 
             <Card.Text>Name: {props.name}</Card.Text>
+
+            {watched.length > 0 ? <Card.Title>Watched: </Card.Title> : null}
+            {watched}
 
         </Card>
     );
